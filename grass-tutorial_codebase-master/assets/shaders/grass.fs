5e540d48787ec9d691624123bb2dae3dc3eb27ba
Ingrid O'Connor <ingridoco@gmail.com>
2023-03-29 11:25:07 -0500

    added LOD logic

--- grass-tutorial_codebase-master/assets/shaders/grass.fs
+++ grass-tutorial_codebase-master/assets/shaders/grass.fs
@@ -4,6 +4,7 @@ out vec4 FragColor;
 
 in GS_OUT {
     vec2 textCoord;
+    float colorVariation;
 } fs_in;
 
 uniform sampler2D u_textgrass;
@@ -11,6 +12,8 @@ uniform sampler2D u_textgrass;
 void main(){
 //    vec4 color = texture(u_textgrass, fs_in.textCoord);
 //    if (color.a < 0.05) discard;
-//	FragColor = color;
-    FragColor = vec4(0.192f, 0.608f, 0.329f, 1.0f);
+    vec4 color = vec4(0.192f, 0.608f, 0.329f, 1.0f);
+    color.xyz = mix(color.xyz, 0.5*color.xyz, fs_in.colorVariation);
+
+    FragColor = color;
 }
